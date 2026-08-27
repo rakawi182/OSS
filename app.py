55fcc853d16e2f4bbeb5d93d989d595743bfd305
rakawi182 <narasatya16@gmail.com>
2026-08-27 09:35:43 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -290,7 +290,7 @@ nav = st.sidebar.radio(
 st.sidebar.markdown("---")
 wib_tz = timezone(timedelta(hours=7))
 now_wib = datetime.now(wib_tz)
-st.sidebar.caption(f"RAKAWI.Ω • {now_wib.strftime('%Y-%m-%d')}")
+st.sidebar.caption(f"PAWITRA • {now_wib.strftime('%Y-%m-%d')}")
 st.sidebar.caption("Open Source • Jolotundo Research Consortium")
 st.sidebar.caption("🏫 Sekolah Alam Penanggungan")
 st.sidebar.caption("📜 SAJAK (Sinau Aksara Jawa Kuno)")
